fd8def2fec9da73e271f24aca541f9e3690a29bf
krdlab <krdlab@gmail.com>
2020-03-23 11:27:42 +0900

    test: Add tests of `generateDiscoveryUrl`

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,10 @@
-import qualified Spec.Client          as Client
-import qualified Spec.Client.Internal as Internal
-import           Test.Hspec           (hspec)
+import qualified Spec.Client           as Client
+import qualified Spec.Client.Discovery as Discovery
+import qualified Spec.Client.Internal  as Internal
+import           Test.Hspec            (hspec)
 
 main :: IO ()
 main = hspec $ do
     Client.tests
+    Discovery.tests
     Internal.tests
