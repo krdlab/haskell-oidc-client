7f84ad07e3284002f7b4d613ce1e317878e08efa
Parnell Springmeyer <parnell@digitalmentat.com>
2020-08-18 19:21:13 -0500

    Add Nix code for nix-shell development environments

---
+++ release.nix
@@ -0,0 +1,33 @@
+let
+  config   = { allowUnfree = true; };
+  overlays = [
+    (newPkgs: oldPkgs: rec {
+
+      haskellPackages = oldPkgs.haskellPackages.override {
+        overrides = haskellPackagesNew: _: {
+          oidc-client = haskellPackagesNew.callCabal2nix "oidc-client" ./. { };
+        };
+      };
+
+    })
+  ];
+
+  nixpkgs = import ./nix/20_03.nix;
+  pkgs    = import nixpkgs { inherit config overlays; };
+
+  oidc-client-shell = pkgs.haskellPackages.shellFor {
+    withHoogle = false;
+    packages = p: [
+      p.oidc-client
+    ];
+
+    buildInputs = [
+      pkgs.ghcid
+    ];
+  };
+
+in
+
+  { inherit (pkgs.haskellPackages) oidc-client;
+    inherit oidc-client-shell;
+  }
