9c7c8ecee6a3dc026baee32fa194762ef95a7e64
Ben Millwood <thebenmachine+git@gmail.com>
2024-12-21 21:43:04 +0000

    Add Provider.validUntil
    This allows long-running services to know how often they may need to
    rerun discovery.

--- src/Web/OIDC/Client/Discovery.hs
+++ src/Web/OIDC/Client/Discovery.hs
@@ -22,11 +22,16 @@ module Web.OIDC.Client.Discovery
 import           Control.Monad.Catch                (catch, throwM)
 import           Data.Aeson                         (eitherDecode)
 import           Data.ByteString                    (append, isSuffixOf)
-import           Data.Monoid                        ((<>))
-import           Data.Text                          (pack)
+import           Data.Foldable                      (foldMap)
+import           Data.Monoid                        ((<>), First (..), getFirst)
+import           Data.Text                          (pack, unpack)
+import           Data.Text.Encoding                 (decodeLatin1)
+import           Data.Time.Format                   (defaultTimeLocale,
+                                                     parseTimeM)
 import qualified Jose.Jwk                           as Jwk
 import           Network.HTTP.Client                (Manager, Request, httpLbs,
-                                                     path, responseBody)
+                                                     path, responseBody,
+                                                     responseHeaders)
 
 import           Web.OIDC.Client.Discovery.Issuers  (google)
 import           Web.OIDC.Client.Discovery.Provider (Configuration (..),
@@ -35,30 +40,55 @@ import           Web.OIDC.Client.Internal           (parseUrl, rethrow)
 import           Web.OIDC.Client.Types              (IssuerLocation,
                                                      OpenIdException (..))
 
--- | This function obtains OpenID Provider configuration and JWK set.
+-- | This function obtains OpenID Provider configuration and JWK set, and infers
+-- 'validUntil' from the HTTP Expires headers.
 discover
     :: IssuerLocation   -- ^ OpenID Provider's Issuer location
     -> Manager
     -> IO Provider
 discover location manager = do
-    conf <- getConfiguration `catch` rethrow
+    (conf, confValidity) <- getConfiguration `catch` rethrow
     case conf of
         Right c   -> do
-            json <- getJwkSetJson (jwksUri c) `catch` rethrow
+            (json, keysValidity) <- getJwkSetJson (jwksUri c) `catch` rethrow
             case jwks json of
-                Right keys -> return $ Provider c keys
+                Right keys -> return $ Provider
+                    { configuration = c
+                    , jwkSet = keys
+                    , validUntil = bothValidUntil confValidity keysValidity
+                    }
                 Left  err  -> throwM $ DiscoveryException ("Failed to decode JwkSet: " <> pack err)
         Left  err -> throwM $ DiscoveryException ("Failed to decode configuration: " <> pack err)
   where
     getConfiguration = do
         req <- generateDiscoveryUrl location
         res <- httpLbs req manager
-        return $ eitherDecode $ responseBody res
+        return (eitherDecode $ responseBody res, getValidity res)
+
+    getValidity res =
+        getFirst $ foldMap (First . getValidityFromHeader) $ responseHeaders res
+
+    getValidityFromHeader (header, value)
+        -- cache-control overrides expires if present, but cache-control
+        -- involves doing calculations with the date and the age header, whereas
+        -- expires is much simpler to use
+        | header == "expires" =
+            parseTimeM True defaultTimeLocale
+              -- https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Expires#syntax
+              "%a, %d %b %Y %H:%M:%S %Z"
+              -- note that headers are in Latin-1 even if the body is encoded
+              -- with something else
+              $ unpack $ decodeLatin1 value
+        | otherwise = Nothing
+
+    bothValidUntil a Nothing = a
+    bothValidUntil Nothing b = b
+    bothValidUntil (Just a) (Just b) = Just (min a b)
 
     getJwkSetJson url = do
         req <- parseUrl url
         res <- httpLbs req manager
-        return $ responseBody res
+        return (responseBody res, getValidity res)
 
     jwks j = Jwk.keys <$> eitherDecode j
 
